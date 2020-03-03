bc030bfb0f8b9e8f53b75f01721bde3ea2da9c37
Ajinkya Bhalerao <bajinkya1996@gmail.com>
2020-03-03 07:06:21 +0530

    Add files via upload

--- ROCK_THE_JVM/src/lectures/part2oop/MethodNotations.scala
+++ ROCK_THE_JVM/src/lectures/part2oop/MethodNotations.scala
@@ -5,7 +5,7 @@ object MethodNotations extends App {
   class Person(val name:String,favoriteMovie :String){
     def likes(movie:String):Boolean = movie==favoriteMovie
     def hangoutWith(person :Person): String = s"${this.name} is hanging out with ${person.name}"
-
+    def apply(): String = s"Hi my name is $name and i like $favoriteMovie"
   }
   val mary = new Person("Mary","Inception")
   println(mary.likes("Inception"))
@@ -16,6 +16,7 @@ object MethodNotations extends App {
   //"operators" in scala
   val tom = new Person("Tom","Fight Club")
   println(mary hangoutWith tom)
+  println(mary())
 
 
 
